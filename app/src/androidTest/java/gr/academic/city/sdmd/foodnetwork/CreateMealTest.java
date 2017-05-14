a5928179bf4df9bd545f681d39b07453026b5a04
Tringa <tringaakrasniqi@gmail.com>
2017-05-15 02:31:31 +0300

    Complete CreateMealTest --Arieta

--- app/src/androidTest/java/gr/academic/city/sdmd/foodnetwork/CreateMealTest.java
+++ app/src/androidTest/java/gr/academic/city/sdmd/foodnetwork/CreateMealTest.java
@@ -1,12 +1,17 @@
 package gr.academic.city.sdmd.foodnetwork;
 
 
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
 import android.widget.TimePicker;
 
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,7 +23,9 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -37,34 +44,35 @@ public class CreateMealTest {
         onView(withId(R.id.txt_recipe)).perform(typeText(recipe), closeSoftKeyboard());
         onView(withId(R.id.txt_number_of_servings)).perform(typeText(noServings), closeSoftKeyboard());
         onView(withId(R.id.tv_prep_time)).perform(click());
-        ViewInteraction perform = onView(isAssignableFrom(TimePicker.class)).perform(click());
-      //  ViewInteraction perform = onView(isAssignableFrom(TimePicker.class)).perform(setTime(0,15));
-       // onView(withId(R.id.btn_ok)).perform(click());
+
+        ViewInteraction perform = onView(isAssignableFrom(TimePicker.class)).perform(setTime(0,15));
+        onView(withText("OK")).perform(click());
+
+        onView(withClassName(Matchers.equalTo(TimePicker.class.getName())));
+        onView(withId(R.id.btn_ok)).perform(click());
 
     }
 
-//public static ViewAction setTime(final int hour, final int minute) {
-//        return new ViewAction() {
-//            @Override
-//            public void perform(UiController uiController, View view) {
-//                TimePicker tp = (TimePicker) view;
-//                tp.setCurrentHour(hour);
-//                tp.setCurrentMinute(minute);
-//            }
-//
-//            @Override
-//            public String getDescription() {
-//                return "Set the passed time into the TimePicker";
-//            }
-//
-//            @Override
-//            public Matcher<View> getConstraints() {
-//                return ViewMatchers.isAssignableFrom(TimePicker.class);
-//            }
-//        };
+public static ViewAction setTime(final int hour, final int minute) {
+    return new ViewAction() {
+        @Override
+        public void perform(UiController uiController, View view) {
+            TimePicker tp = (TimePicker) view;
+            tp.setCurrentHour(hour);
+            tp.setCurrentMinute(minute);
+        }
 
+        @Override
+        public String getDescription() {
+            return "Set the passed time into the TimePicker";
+        }
 
-}
+        @Override
+        public Matcher<View> getConstraints() {
+            return isAssignableFrom(TimePicker.class);
+        }
+    };
 
+}}
 
 
